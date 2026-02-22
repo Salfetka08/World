b2351cb8e2c87bab7b1e9b0271cb4028211746fe
Salfetka08 <ru.gareeva@gmail.com>
2026-02-23 00:01:19 +0300

    Revert "Добавление Swagger документации."
    This reverts commit 1265ef14427d44486f5574171e09d625fee5ea3c.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\MemoryWeatherController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
-    Route::prefix('weather')->group(function () {
-        Route::get('random-idea', [MemoryWeatherController::class, 'randomIdea']);
-    });
+Route::get('/', function () {
+    return view('welcome');
 });
